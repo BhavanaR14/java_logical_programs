85bf507752593a5f23caf2322edd864b3d38f478
BhavanaR14 <rasupallibhavana@gmail.com>
2024-11-06 03:12:57 -0800

    Add files via upload

---
+++ DAY-1/Third.java
@@ -0,0 +1,13 @@
+class Third
+{
+public static void main(String args[])
+{
+int eno=101;
+String ename="Bhavana";
+float esal=544.45F;
+ 
+System.out.println("The given employee Number is:"+eno);
+System.out.println("The given employee Name is:"+ename);
+System.out.println("The given employee Salary is:"+esal);
+}
+}
